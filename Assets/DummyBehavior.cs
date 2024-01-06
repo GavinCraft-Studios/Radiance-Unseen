cfc14f7dc0d45a1818113638eba013f6d226c706
GavinCraft-Studios <gavincraft694@gmail.com>
2024-01-06 07:55:45 -0500

    Transfer to Laptop
    Pause OS in Development

---
+++ Assets/DummyBehavior.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DummyBehavior : MonoBehaviour
+{
+    [Header("Animation")]
+    public List<Sprite> nonHit;
+    public List<Sprite> hit;
+
+    private int animationState;
+
+    private EnemyHealthManager enemyHealthManager;
+    private SpriteRenderer spriteRenderer;
+
+    void Awake()
+    {
+        enemyHealthManager = GetComponent<EnemyHealthManager>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        animationState = 0;
+    }
+
+    void Start()
+    {
+        StartCoroutine(anim());
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        spriteRenderer.sprite = hit[animationState];
+    }
+
+    void Update()
+    {
+        if (enemyHealthManager.enemyHealth != 1)
+        {
+            // health changed
+            spriteRenderer.sprite = hit[animationState];
+        }
+        enemyHealthManager.enemyHealth = 1;
+    }
+
+    IEnumerator anim()
+    {
+        if(animationState > -1 && animationState < 4) {spriteRenderer.sprite = nonHit[animationState];}
+        yield return new WaitForSeconds(0.25f);
+        animationState += 1;
+        if (animationState == 4)
+        {
+            animationState = 0;
+        }
+        StartCoroutine(anim());
+    }
+}
--- Assets/Scripts/UI + Camera/Pause OS/PauseOS_Controller.cs
+++ Assets/Scripts/UI + Camera/Pause OS/PauseOS_Controller.cs
@@ -1,16 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PauseOS_Controller : MonoBehaviour
 {
+    [Header("State")]
     public bool isOpen;
+    public bool isOpenUpdated;
+
+    [Header("References")]
+    public CanvasGroup cg;
+    public SpriteRenderer ocRend;
 
     private KeycodeDatabase keycodeDatabase;
     private Dictionary<int, KeyCode> keybinds;
 
-    public float updateRate = 0.5f;
+    [Header("Config")]
+    public float updateRate = 1.1f;
     private float lastUpdate = 0f;
+
+    [Header("Animation")]
+    public List<Sprite> openclose;
+    public List<Sprite> closeopen;
+    public float ocAnimTime;
 
     void Update()
     {
@@ -20,25 +33,72 @@ public class PauseOS_Controller : MonoBehaviour
         }
         this.keybinds = keycodeDatabase.GetFullDictionary();
 
-        if (isOpen && Input.GetKey(keybinds[9]) && Time.time > updateRate + lastUpdate)
+        if (isOpen && Input.GetKey(keybinds[9]) && Time.realtimeSinceStartup > updateRate + lastUpdate)
         {
             isOpen = false;
-            reloadOS();
-            lastUpdate = Time.time;
+            isOpenUpdated = true;
+            StartCoroutine(reloadOS());
+            lastUpdate = Time.realtimeSinceStartup;
         }
-        else if (Input.GetKey(keybinds[9]) && Time.time > updateRate + lastUpdate)
+        else if (Input.GetKey(keybinds[9]) && Time.realtimeSinceStartup > updateRate + lastUpdate)
         {
             isOpen = true;
-            reloadOS();
-            lastUpdate = Time.time;
+            isOpenUpdated = true;
+            StartCoroutine(reloadOS());
+            lastUpdate = Time.realtimeSinceStartup;
         }
+
+        Debug.Log(Time.timeScale.ToString());
     }
 
-    public void reloadOS()
+    IEnumerator reloadOS()
     {
         if (isOpen)
         {
+            if (isOpenUpdated)
+            {
+                StartCoroutine(ocAnim(false));
+                for (int i = 1; i <= 100; i++)
+                {
+                    Time.timeScale = i / 100;
+                    yield return new WaitForSecondsRealtime(ocAnimTime / 100);
+                }
+            }
+        }
+        else
+        {
+            if (isOpenUpdated)
+            {
+                StartCoroutine(ocAnim(true));
+                for (int i = 100; i > 1; i--)
+                {
+                    Time.timeScale = i / 100;
+                    yield return new WaitForSecondsRealtime(ocAnimTime / 100);
+                }
+            }
+        }
 
+        isOpenUpdated = false;
+        yield return new WaitForSecondsRealtime(0.01f);
+    }
+
+    IEnumerator ocAnim(bool isOpening)
+    {
+        if (isOpening)
+        {
+            foreach (Sprite frame in openclose)
+            {
+                yield return new WaitForSecondsRealtime(ocAnimTime / openclose.Count);
+                ocRend.sprite = frame;
+            }
+        }
+        else
+        {
+            foreach (Sprite frame in closeopen)
+            {
+                yield return new WaitForSecondsRealtime(ocAnimTime / closeopen.Count);
+                ocRend.sprite = frame;
+            }
         }
     }
 }
