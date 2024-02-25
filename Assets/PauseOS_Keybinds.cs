4bc50ec67ed4088b3d39712e4ff3ff0a9692d855
Gavin D.B <gavincraft694@gmail.com>
2024-02-25 09:03:24 -0500

    Transfer to Laptop for Bug Fixing

--- Assets/PauseOS_Keybinds.cs
+++ Assets/PauseOS_Keybinds.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine;
 using TMPro;
 
@@ -10,4 +11,34 @@ public class PauseOS_Keybinds : MonoBehaviour
     
     public int dataSelector;
     private KeycodeDatabase keycodeDatabase;
+
+    void Start()
+    {
+        keycodeDatabase = GameObject.Find("Player").GetComponent<KeycodeDatabase>();
+
+        selectedKeyCode = keycodeDatabase.GetKeycodeInDatabase(dataSelector);
+        buttonLabel.text = selectedKeyCode.ToString();
+    }
+
+    public void ChangeKey()
+    {
+        buttonLabel.text = "-<{ }>-";
+        buttonLabel.color = new Color32(64, 138, 255, 255);
+    }
+
+    void Update()
+    {
+        if (buttonLabel.text == "-<{ }>-") {
+            foreach (KeyCode keyCode in Enum.GetValues(typeof(KeyCode))) {
+                if (Input.GetKey(keyCode)) {
+                    // Update Keycode
+                    buttonLabel.text = keyCode.ToString();
+                    selectedKeyCode = keyCode;
+                    keycodeDatabase.SetKeycodeInDatabase(dataSelector, selectedKeyCode);
+
+                    buttonLabel.color = new Color32(255, 255, 255, 255);
+                }
+            }
+        }
+    }
 }
--- Assets/Scripts/Audio/AudioManager.cs
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -31,7 +31,7 @@ public class AudioManager : MonoBehaviour, IDataPersistance
     //private Scrollbar ambienceScroll;
     private Scrollbar musicScroll;
     private Scrollbar sfxScroll;
-    private Scrollbar voiceScroll;
+    //private Scrollbar voiceScroll;
 
     private List<EventInstance> eventInstances;
     private List<StudioEventEmitter> eventEmitters;
@@ -53,9 +53,9 @@ public class AudioManager : MonoBehaviour, IDataPersistance
 
         masterBus = RuntimeManager.GetBus("bus:/");
         musicBus = RuntimeManager.GetBus("bus:/Music");
-        ambienceBus = RuntimeManager.GetBus("bus:/Ambience");
+        //ambienceBus = RuntimeManager.GetBus("bus:/Ambience");
         sfxBus = RuntimeManager.GetBus("bus:/SFX");
-        voiceBus = RuntimeManager.GetBus("bus:/Voice");
+        //voiceBus = RuntimeManager.GetBus("bus:/Voice");
     }
 
     private void Start()
@@ -76,13 +76,13 @@ public class AudioManager : MonoBehaviour, IDataPersistance
         //ambienceScroll = GameObject.Find("Ambience Vol.").GetComponent<Scrollbar>();
         musicScroll = GameObject.Find("Music Vol.").GetComponent<Scrollbar>();
         sfxScroll = GameObject.Find("SFX Vol.").GetComponent<Scrollbar>();
-        voiceScroll = GameObject.Find("Voice Vol.").GetComponent<Scrollbar>();
+        //voiceScroll = GameObject.Find("Voice Vol.").GetComponent<Scrollbar>();
 
         masterScroll.value = data.masterVolume;
         //ambienceScroll.value = data.ambienceVolume;
         musicScroll.value = data.musicVolume;
         sfxScroll.value = data.sfxVolume;
-        voiceScroll.value = data.voiceVolume;
+        //voiceScroll.value = data.voiceVolume;
     }
 
     public void SaveGame(GameData data)
@@ -91,7 +91,7 @@ public class AudioManager : MonoBehaviour, IDataPersistance
         //data.ambienceVolume = this.ambienceVolume;
         data.musicVolume = this.musicVolume;
         data.sfxVolume = this.sfxVolume;
-        data.voiceVolume = this.voiceVolume;
+        //data.voiceVolume = this.voiceVolume;
     }
 
     // ---------------------------------
@@ -102,13 +102,13 @@ public class AudioManager : MonoBehaviour, IDataPersistance
         //ambienceVolume = ambienceScroll.value;
         musicVolume = musicScroll.value;
         sfxVolume = sfxScroll.value;
-        voiceVolume = voiceScroll.value; 
+        //voiceVolume = voiceScroll.value; 
 
         masterBus.setVolume(masterVolume);
         //ambienceBus.setVolume(ambienceVolume);
         musicBus.setVolume(musicVolume);
         sfxBus.setVolume(sfxVolume);
-        voiceBus.setVolume(voiceVolume);
+        //voiceBus.setVolume(voiceVolume);
     }
 
     private void InitializeAmbience(EventReference ambienceEventReference)
